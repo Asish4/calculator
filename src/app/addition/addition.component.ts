3e63b8d0f1783cba27209a2a3685025cb8ef8ab4
Asish Patra <asishpatra34@gmail.com>
2023-03-03 16:05:56 +0530

    Write the logic of addition

--- src/app/addition/addition.component.ts
+++ src/app/addition/addition.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './addition.component.html',
   styleUrls: ['./addition.component.css'],
 })
-export class AdditionComponent {}
+export class AdditionComponent {
+  result: any;
+  add() {
+    const first = document.getElementById('firstNumber') as HTMLInputElement;
+    const firstValue = first.value;
+
+    const second = document.getElementById('secondNumber') as HTMLInputElement;
+    const secondValue = second.value;
+
+    this.result = Number(firstValue) + Number(secondValue);
+    return this.result;
+  }
+}
