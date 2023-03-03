2b2b2e74a4e77a70dfaa268831f9f72f3e0d27c1
Asish Patra <asishpatra34@gmail.com>
2023-03-03 16:18:49 +0530

    Write the logic of div and mul

--- src/app/division/division.component.ts
+++ src/app/division/division.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-division',
   templateUrl: './division.component.html',
-  styleUrls: ['./division.component.css']
+  styleUrls: ['./division.component.css'],
 })
 export class DivisionComponent {
+  result: any;
+  div() {
+    const first = document.getElementById('firstNumber') as HTMLInputElement;
+    const firstValue = first.value;
 
+    const second = document.getElementById('secondNumber') as HTMLInputElement;
+    const secondValue = second.value;
+
+    this.result = Number(firstValue) / Number(secondValue);
+    return this.result;
+  }
 }
--- src/app/multiplication/multiplication.component.ts
+++ src/app/multiplication/multiplication.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-multiplication',
   templateUrl: './multiplication.component.html',
-  styleUrls: ['./multiplication.component.css']
+  styleUrls: ['./multiplication.component.css'],
 })
 export class MultiplicationComponent {
+  result: any;
+  mul() {
+    const first = document.getElementById('firstNumber') as HTMLInputElement;
+    const firstValue = first.value;
 
+    const second = document.getElementById('secondNumber') as HTMLInputElement;
+    const secondValue = second.value;
+
+    this.result = Number(firstValue) * Number(secondValue);
+    return this.result;
+  }
 }
