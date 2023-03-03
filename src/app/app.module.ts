ef78b77c85b39b20b1390783098483bd95f2b5c7
Asish Patra <asishpatra34@gmail.com>
2023-03-03 10:16:52 +0530

    Add more oprerations to sidebar and create a new component called 'addition' and add a header to that comonent also

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AdditionComponent } from './addition/addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    AdditionComponent
   ],
   imports: [
     BrowserModule,
