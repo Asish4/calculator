497cbd8fa8bbb2918df96473c09e406615370d72
Asish Patra <asishpatra34@gmail.com>
2023-03-03 16:12:07 +0530

    Write the logic of subtraction

--- src/app/substraction/substraction.component.ts
+++ src/app/substraction/substraction.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-substraction',
   templateUrl: './substraction.component.html',
-  styleUrls: ['./substraction.component.css']
+  styleUrls: ['./substraction.component.css'],
 })
 export class SubstractionComponent {
+  result: any;
+  sub() {
+    const first = document.getElementById('firstNumber') as HTMLInputElement;
+    const firstValue = first.value;
 
+    const second = document.getElementById('secondNumber') as HTMLInputElement;
+    const secondValue = second.value;
+
+    this.result = Number(firstValue) - Number(secondValue);
+    return this.result;
+  }
 }
