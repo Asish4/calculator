b4f432b33f66fb0a173a39e216b28971c1fae1ab
Asish Patra <asishpatra34@gmail.com>
2023-03-03 12:29:26 +0530

    set routing for 'Division' component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdditionComponent } from './addition/addition.component';
 import { SubstractionComponent } from './substraction/substraction.component';
 import { MultiplicationComponent } from './multiplication/multiplication.component';
+import { DivisionComponent } from './division/division.component';
 
 const routes: Routes = [
   { path: 'addition', component: AdditionComponent },
   { path: 'subtraction', component: SubstractionComponent },
   { path: 'multiplication', component: MultiplicationComponent },
+  { path: 'division', component: DivisionComponent },
 ];
 
 @NgModule({
